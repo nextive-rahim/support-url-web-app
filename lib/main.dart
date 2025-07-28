680f33c8e9accd496b892337443ac3d6fffe6e0b
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2025-07-28 14:46:58 +0600

    flutter build web

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Support Section'),
+      home: const MyHomePage(title: 'Privacy Policies'),
     );
   }
 }
