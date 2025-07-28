1c276c6e3bb89cc3c0bd811802ddd89cd3344bb9
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2025-07-28 13:50:28 +0600

    Chore : Added etestPaper support url

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      title: 'Science Platform',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'eTestPaper',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -54,8 +55,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -74,31 +73,32 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('''  Support – Science Platform App
-        Welcome to the Science Platform Support Page
+      body: SingleChildScrollView(
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //
+              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+              // action in the IDE, or press "p" in the console), to see the
+              // wireframe for each widget.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('''         Support – eTestPaper App
+        Welcome to the eTestPaper Support Page
         
-        We’re here to help you with any questions or issues related to the Science Platform app. Below you’ll find resources and contact information for quick assistance.
+        We’re here to help you with any questions or issues related to the eTestPaper app. Below you’ll find resources and contact information for quick assistance.
         
         🔧 Common Troubleshooting
         App Not Opening
@@ -112,17 +112,15 @@ class _MyHomePageState extends State<MyHomePage> {
         
         Email: rahim.nextive@gmail.com
         Phone: +8801718663032
-        
+         
         📚 FAQs
         Is the app free to use?
-        Yes, the Science Platform app is completely free.
+        Yes, the eTestPaper app is completely free.
         Can I use the app offline?
         Currently, an internet connection is required to access most features.
         Where can I give feedback?
         Email us at rahim.nextive@gmail.com
-        🔒 Privacy Policy
-        Read our privacy policy here:
-        https://science-platform.apphq.online/privacy
+       
         
         Name: Abdul Rahim
         Company: Nextive Solution
@@ -132,12 +130,12 @@ class _MyHomePageState extends State<MyHomePage> {
         Email: rahim.nextive@gmail.com
         
         '''),
-             
-            ],
+              ],
+            ),
           ),
         ),
       ),
-     // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
