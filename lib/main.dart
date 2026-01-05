1a409ef726cd42a8595f8370c7f415712bff71c2
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2026-01-05 15:29:22 +0600

    sahittyapara privacy policy

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Master Class',
+      title: 'Sahittyapara',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  'Master Class',
+                  'Sahittyapara',
                   style: TextStyle(
                     fontSize: 26,
                     fontWeight: FontWeight.w600,
