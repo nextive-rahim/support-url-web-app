ff0152b9004f7c8cd8dff90d65d326dbba2fae57
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2026-04-01 12:40:45 +0600

    privacy

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "515322840"
+    serviceWorkerVersion: "597572129"
   }
 });
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Jony Law School',
+      title: 'English Fantasy',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  'Jony Law School',
+                  'English Fantasy',
                   style: TextStyle(
                     fontSize: 26,
                     fontWeight: FontWeight.w600,
